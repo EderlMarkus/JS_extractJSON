7d99d4ae1c2155b18980cc962bc0a5600b2b1013
EderlMarkus <38421306+EderlMarkus@users.noreply.github.com>
2022-02-04 10:51:35 +0100

    fixed singlequote bug

--- package/index.js
+++ package/index.js
@@ -4,22 +4,24 @@
  * @param {int} levels how many levels in json should be searched?
  * @returns {array} array of jsons found in string
  */
-function extractJSON(str, levels = 4) {
-  let reg = '{(?:[^{}]|({(?:[^{}]|())*}))*}';
+ function extractJSON(str, levels = 4) {
+  let reg = "{(?:[^{}]|({(?:[^{}]|())*}))*}";
   for (let index = 1; index < levels; index++) {
-    reg = reg.replace('()', '{(?:[^{}]|())*}');
+    reg = reg.replace("()", "{(?:[^{}]|())*}");
   }
-  reg = new RegExp(reg, 'g');
+  reg = new RegExp(reg, "g");
   let matches = str.match(reg);
   if (matches) {
     matches = matches.map((match) => {
       try {
         //sanatize whitespaces
-        match = match.replace(/(?<={|,)([\s]+)(?=(.*?)(:))/g, '');
+        match = match.replace(/(?<={|,)([\s]+)(?=(.*?)(:))/g, "");
         //sanatize "relaxed" JSON without quotes
+        const hasSingleQuotes = match.match(/(,)(.*?)(:)(|\s)(')/);
+        const replaceText = hasSingleQuotes ? "'$4': " : '"$4": ';
         match = match.replace(
           /(?<=([{,])(|\s))(['"])?([a-z0-9A-Z_]+)(['"])?:/g,
-          '"$4": '
+          replaceText
         );
         return JSON.parse(match);
       } catch (error) {
